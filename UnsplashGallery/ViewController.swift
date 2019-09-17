0516770018551cb1c867bc6619dbae84c02b7736
Georgij Emelyanov <porox9696@mail.ru>
2019-09-17 20:41:55 +0300

    Initial Commit

---
+++ UnsplashGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnsplashGallery
+//
+//  Created by Georgij Emelyanov on 17/09/2019.
+//  Copyright © 2019 Georgij Emelyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
